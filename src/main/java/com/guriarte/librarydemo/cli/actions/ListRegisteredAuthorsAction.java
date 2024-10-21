a4a3b343cf0541c80318fc82a7c4d67fb2aeb09a
g-uriarte <g.uri.duck@proton.me>
2024-10-21 12:15:50 -0300

    feat: list registered authors

--- src/main/java/com/guriarte/librarydemo/cli/actions/ListRegisteredAuthorsAction.java
+++ src/main/java/com/guriarte/librarydemo/cli/actions/ListRegisteredAuthorsAction.java
@@ -1,10 +1,16 @@
 package com.guriarte.librarydemo.cli.actions;
 
 import com.guriarte.librarydemo.errors.LibraryBaseException;
+import com.guriarte.librarydemo.library.service.AuthorService;
 
 public class ListRegisteredAuthorsAction implements Action {
 
     public static final int OPTION = 3;
+    private final AuthorService authorService;
+
+    public ListRegisteredAuthorsAction(AuthorService authorService) {
+        this.authorService = authorService;
+    }
 
     @Override
     public int option() {
@@ -18,6 +24,13 @@ public class ListRegisteredAuthorsAction implements Action {
 
     @Override
     public void perform() throws LibraryBaseException {
-
+        var optionalAuthors =  this.authorService.getAll();
+        if (optionalAuthors.isPresent()) {
+            optionalAuthors
+                .get()
+                .forEach(author -> System.out.println(author.getId() + ". " + author.getName() + " " + author.getBirthYear() + " - " + author.getDeathYear()));
+        } else {
+            System.out.println("Not found registered authors.");
+        }
     }
 }
