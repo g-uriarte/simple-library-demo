2891e256fd2dd2a744f766ef380f879ee1539bfa
g-uriarte <g.uri.duck@proton.me>
2024-10-21 12:10:04 -0300

    feat: list registered books

--- src/main/java/com/guriarte/librarydemo/cli/actions/ListRegisteredBooksAction.java
+++ src/main/java/com/guriarte/librarydemo/cli/actions/ListRegisteredBooksAction.java
@@ -1,10 +1,17 @@
 package com.guriarte.librarydemo.cli.actions;
 
 import com.guriarte.librarydemo.errors.LibraryBaseException;
+import com.guriarte.librarydemo.library.service.BookService;
 
 public class ListRegisteredBooksAction implements Action {
 
     public static final int OPTION = 2;
+
+    private final BookService bookService;
+
+    public ListRegisteredBooksAction(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @Override
     public int option() {
@@ -18,6 +25,13 @@ public class ListRegisteredBooksAction implements Action {
 
     @Override
     public void perform() throws LibraryBaseException {
+        var optionalBooks = this.bookService.getAllBooks();
+
+        if (optionalBooks.isPresent()) {
+            optionalBooks.get().forEach(book -> System.out.println(book.getId() + ". " + book.getTitle()));
+        } else {
+            System.out.println("Not found registered books. Please register a book");
+        }
 
     }
 }
