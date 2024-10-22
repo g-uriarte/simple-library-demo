03f52b2a6725f4152d268224205fb9d583c42787
g-uriarte <g.uri.duck@proton.me>
2024-10-22 13:18:33 -0300

    feat: add header table to book list message

--- src/main/java/com/guriarte/librarydemo/cli/actions/common/messages/ListBooksMessage.java
+++ src/main/java/com/guriarte/librarydemo/cli/actions/common/messages/ListBooksMessage.java
@@ -4,19 +4,20 @@ import com.guriarte.librarydemo.utils.Text;
 
 import java.util.List;
 
-public class ListBookMessage {
+public class ListBooksMessage {
 
     private final List<SimpleBook> books;
-    private static final String BOOK_FORMAT = "%d . %s";
+    private static final String BOOK_FORMAT = "%d.\t%s";
     private static final String BOOKS_FOUND_FORMAT = "Books found (%s): ";
 
-    public ListBookMessage(List<SimpleBook> books) {
+    public ListBooksMessage(List<SimpleBook> books) {
         this.books = books;
     }
 
     @Override
     public String toString() {
         var text = new Text();
+        text.appendln("Id \t Title");
         text.appendln(String.format(BOOKS_FOUND_FORMAT, books.size()));
         for (int i = 0; i < books.size(); i++) {
             text.appendln(String.format(BOOK_FORMAT, i + 1, books.get(i).title()));
